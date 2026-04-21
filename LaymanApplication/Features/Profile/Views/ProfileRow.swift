a9c3420d1f99b4e2226fdef4f77fc70073368182
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:10:43 +0530

    Update ProfileRow.swift

--- LaymanApplication/Features/Profile/Views/ProfileRow.swift
+++ LaymanApplication/Features/Profile/Views/ProfileRow.swift
@@ -5,4 +5,25 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProfileRow: View {
+    
+    let icon: String
+    let title: String
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            
+            Image(systemName: icon)
+                .foregroundColor(.orange)
+            
+            Text(title)
+            
+            Spacer()
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(12)
+    }
+}
