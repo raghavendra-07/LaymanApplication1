f0ecb63c4598e5d0f78eb23262b8b5a945e2d584
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:57:41 +0530

    Update SampleData.swift

--- LaymanApplication/Models/SampleData.swift
+++ LaymanApplication/Models/SampleData.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+let sampleContent = [
+    "This company is building smarter AI tools that can talk and think better. They want to compete with big players like OpenAI and Google in a simple way.",
+    
+    "They raised billions of dollars to improve their technology. The goal is to make AI more useful for everyday people, not just big companies.",
+    
+    "If successful, this could change how we use AI daily. It might become faster, cheaper, and more accessible to everyone."
+]
+
