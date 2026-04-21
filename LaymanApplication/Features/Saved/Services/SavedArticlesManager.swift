696b597f4a8249d8dd8185a09017d169c5d603de
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:02:44 +0530

    Update SavedArticlesManager.swift

--- LaymanApplication/Features/Saved/Services/SavedArticlesManager.swift
+++ LaymanApplication/Features/Saved/Services/SavedArticlesManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+class SavedArticlesManager: ObservableObject {
+    
+    static let shared = SavedArticlesManager()
+    
+    @Published var savedArticles: [NewsArticle] = []
+    
+    func toggleSave(article: NewsArticle) {
+        if isSaved(article) {
+            savedArticles.removeAll { $0.id == article.id }
+        } else {
+            savedArticles.append(article)
+        }
+    }
+    
+    func isSaved(_ article: NewsArticle) -> Bool {
+        savedArticles.contains { $0.id == article.id }
+    }
+}
+
