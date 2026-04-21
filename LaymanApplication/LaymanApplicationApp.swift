5af9e682833c14e68836354817add5012e0caf73
raghavendra-07 <raghavendrachary010@gmail.com>
2026-04-21 10:02:18 +0530

    Initial Commit

---
+++ LaymanApplication/LaymanApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApplicationApp.swift
+//  LaymanApplication
+//
+//  Created by V Raghavendra Chary on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
