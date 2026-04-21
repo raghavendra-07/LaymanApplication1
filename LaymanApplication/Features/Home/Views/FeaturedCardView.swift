206d9aee6351e61fdd7355ab5134dfdbeb818098
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:09:29 +0530

    Update FeaturedCardView.swift

--- LaymanApplication/Features/Home/Views/FeaturedCardView.swift
+++ LaymanApplication/Features/Home/Views/FeaturedCardView.swift
@@ -5,4 +5,37 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct FeaturedCardAPI: View {
+    let article: NewsArticle
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            
+            AsyncImage(url: URL(string: article.image_url ?? "")) { image in
+                image.resizable()
+            } placeholder: {
+                Color.gray
+            }
+            .scaledToFill()
+            .frame(height: 220)
+            .clipped()
+            .cornerRadius(20)
+            
+            LinearGradient(
+                colors: [.clear, .black.opacity(0.7)],
+                startPoint: .center,
+                endPoint: .bottom
+            )
+            .cornerRadius(20)
+            
+            Text(article.title)
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+        }
+        .padding(.horizontal)
+    }
+}
+
