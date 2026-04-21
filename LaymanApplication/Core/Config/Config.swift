76329423cefc6851061b88153e41fa772be53b6b
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:39:32 +0530

    Update Config.swift

--- LaymanApplication/Core/Config/Config.swift
+++ LaymanApplication/Core/Config/Config.swift
@@ -11,6 +11,6 @@ struct Config {
     static let supabaseURL = "https://wtfcmcrfhwpdxpijtuoi.supabase.co"
     static let supabaseKey = "sb_publishable_RK3D9yMRqZJkYDCxe6TB6Q_8xSpPBxw"
     static let newsAPIKey = "pub_8562ff4537b54999822c8d38382066cb"
-    static let geminiAPIKey = "AQ.Ab8RN6ItC9UchbKrlHu7QgtH8LdN4LNbKqDLrZKORfFIxb0plA"
+    static let geminiAPIKey = "AIzaSyD91fpHCSs_IcSoM1FToc6kWZEwbJwqE2U"
 }
 
