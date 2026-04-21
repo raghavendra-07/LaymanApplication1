3c48dd713d4bfed62a4bc1f090448566676e77d9
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:55:08 +0530

    Update HomeViewModel.swift

--- LaymanApplication/Features/Home/ViewModels/HomeViewModel.swift
+++ LaymanApplication/Features/Home/ViewModels/HomeViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Combine   //  THIS WAS MISSING
+
+@MainActor
+class HomeViewModel: ObservableObject {
+    
+    @Published var articles: [NewsArticle] = []
+    @Published var isLoading = false
+    
+    private let service = NewsService()
+    
+    func loadArticles() {
+        Task {
+            do {
+                isLoading = true
+                articles = try await service.fetchArticles()
+                isLoading = false
+            } catch {
+                print("Error fetching:", error)
+                isLoading = false
+            }
+        }
+    }
+}
+
