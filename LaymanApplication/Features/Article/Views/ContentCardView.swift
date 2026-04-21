68fe2c865bc281eb97b3e72bd5917c9077a69b65
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:04:57 +0530

    Update ContentCardView.swift

--- LaymanApplication/Features/Article/Views/ContentCardView.swift
+++ LaymanApplication/Features/Article/Views/ContentCardView.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ContentCard: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.subheadline)
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(.systemGray6))
+            .cornerRadius(16)
+    }
+}
