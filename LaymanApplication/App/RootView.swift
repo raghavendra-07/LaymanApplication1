bc0b34e08f924e4f66632d4de0d4fe2af7439367
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:45:36 +0530

    Update RootView.swift

--- LaymanApplication/App/RootView.swift
+++ LaymanApplication/App/RootView.swift
@@ -5,4 +5,23 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct RootView: View {
+    
+    @State private var hasSeenWelcome = false
+    @StateObject private var vm = AuthViewModel()
+    
+    var body: some View {
+        
+        if !hasSeenWelcome {
+            WelcomeView(hasSeenWelcome: $hasSeenWelcome)
+            
+        } else if vm.isLoggedIn {
+            ContentView(vm: vm)
+            
+        } else {
+            AuthView(vm: vm)
+        }
+    }
+}
