86ffeb8b8c63f3ba34062514861f12b1f05ac6e9
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:05:43 +0530

    Update SafariView.swift

--- LaymanApplication/Core/Utilities/SafariView.swift
+++ LaymanApplication/Core/Utilities/SafariView.swift
@@ -5,4 +5,18 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+import SafariServices
+
+//MARK: SafariView
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
+}
+
