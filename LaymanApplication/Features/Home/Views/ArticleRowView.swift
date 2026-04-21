b6e1c8f14ba9e79ecd92ed48c422ad6ebb36e373
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 11:10:03 +0530

    Update ArticleRowView.swift

--- LaymanApplication/Features/Home/Views/ArticleRowView.swift
+++ LaymanApplication/Features/Home/Views/ArticleRowView.swift
@@ -5,4 +5,54 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct ArticleRowAPI: View {
+    let article: NewsArticle
+    
+    var body: some View {
+//        HStack(spacing: 12) {
+//            
+//            AsyncImage(url: URL(string: article.image_url ?? "")) { image in
+//                image.resizable()
+//            } placeholder: {
+//                Color.gray
+//            }
+//            .frame(width: 70, height: 70)
+//            .cornerRadius(12)
+//            
+//            Text(article.title)
+//                .font(.subheadline)
+//                .lineLimit(2)
+//            
+//            Spacer()
+//        }
+//        .padding(10)
+//        .background(Color(.systemGray6))
+//        .cornerRadius(12)
+        NavigationLink {
+            ArticleDetailView(article: article)
+        } label: {
+            HStack(spacing: 12) {
+                
+                AsyncImage(url: URL(string: article.image_url ?? "")) { image in
+                    image.resizable()
+                } placeholder: {
+                    Color.gray
+                }
+                .frame(width: 70, height: 70)
+                .cornerRadius(12)
+                
+                Text(article.title)
+                    .font(.subheadline)
+                    .lineLimit(2)
+                
+                Spacer()
+            }
+            .padding(10)
+            .background(Color(.systemGray6))
+            .cornerRadius(12)
+        }
+    }
+}
+
