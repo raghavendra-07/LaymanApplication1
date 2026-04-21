6826f0f7a08debe4b6fe7f0c67839d0fd85baff9
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:59:25 +0530

    Update NewsService.swift

--- LaymanApplication/Core/Networking/NewsService.swift
+++ LaymanApplication/Core/Networking/NewsService.swift
@@ -5,4 +5,24 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
+
 import Foundation
+
+class NewsService {
+    
+    func fetchArticles() async throws -> [NewsArticle] {
+        
+        let urlString = "https://newsdata.io/api/1/news?apikey=\(Config.newsAPIKey)&category=technology,business"
+        
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decoded = try JSONDecoder().decode(NewsResponse.self, from: data)
+        
+        return decoded.results
+    }
+}
+
