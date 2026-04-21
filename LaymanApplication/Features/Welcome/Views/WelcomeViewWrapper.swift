27975286cc93005f95d7db7c81e9731e428a9197
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:51:02 +0530

    Update WelcomeViewWrapper.swift

--- LaymanApplication/Features/Welcome/Views/WelcomeViewWrapper.swift
+++ LaymanApplication/Features/Welcome/Views/WelcomeViewWrapper.swift
@@ -5,4 +5,16 @@
 //  Created by V Raghavendra Chary on 21/04/26.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct WelcomeViewWrapper: View {
+    @Binding var hasSeenWelcome: Bool
+    
+    var body: some View {
+        WelcomeView(hasSeenWelcome: $hasSeenWelcome)
+            .onDisappear {
+                hasSeenWelcome = true
+            }
+    }
+}
