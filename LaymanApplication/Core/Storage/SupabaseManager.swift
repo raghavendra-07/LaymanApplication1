50a05a2732b59ca38b11b1841cb8a26cc48cf692
raghavendra-07 <98819082+raghavendra-07@users.noreply.github.com>
2026-04-21 10:47:56 +0530

    Update SupabaseManager.swift

--- LaymanApplication/Core/Storage/SupabaseManager.swift
+++ LaymanApplication/Core/Storage/SupabaseManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Supabase
+
+final class SupabaseManager {
+    
+    static let shared = SupabaseManager()
+    
+    let client: SupabaseClient
+    
+    private init() {
+        client = SupabaseClient(
+            supabaseURL: URL(string: Config.supabaseURL)!,
+            supabaseKey: Config.supabaseKey
+        )
+    }
+}
+
